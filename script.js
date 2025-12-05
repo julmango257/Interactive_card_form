0abaa01de88da84e74d189d589880b0708bded75
Afifa <afifafatema@hotmail.com>
2025-12-05 16:35:50 +0000

    script updates

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById ("form");
+const name = document.getElementById("name");
+const number = document.getElementById("number");
+const expiryMonth = document.getElementById("expiry-month");
+const expiryYear = document.getElementById ("expiry-year");
+const cvc = document.getElementById("cvc");
+const confirm = document.getElementById("confirm");
+
+
+form.addEventListener("submit", e =>{
+    e.preventDefault();
+    validateInputs;
+});
+
+const validateInputs = () => {
+    const nameValue = name.value.trim();
+    
+}
